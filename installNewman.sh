6969ce9709a9d9ec0d6ab020bf5ea7e250811cec
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-02-27 13:34:26 +0530

    Update installNewman.sh

--- installNewman.sh
+++ installNewman.sh
@@ -1,4 +1,8 @@
 sudo apt update
-sudo apt install nodejs npm -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
 nodejs --version
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+npm --version
 sudo npm install -g newman
